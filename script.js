5c4101ac3d5746cb61c91ce9b0c95f2848efffd2
Dayanna R <dayi2007@hotmail.com>
2021-10-04 21:46:41 -0400

    buttons

--- script.js
+++ script.js
@@ -78,3 +78,15 @@ btn.addEventListener("click", (event) => {
     document.querySelector("#look-ingredients").value = " ";
 
 });
+
+
+/*
+checkbox and submit
+
+ const cb = document.querySelector('#accept');
+        const btn = document.querySelector('#btn');
+        btn.onclick = () => {
+            const result = cb.value;
+            alert(result); // on
+        };
+*/
